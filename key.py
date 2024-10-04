361bc0c3821925cda587e46cb89107b3ddbcd6e5
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-04 13:30:34 +0600

    Added key.py

---
+++ key.py
@@ -0,0 +1,11 @@
+#Here I need to know how the key actually works
+
+#lets have key and values in a dictionary
+
+d={'Azmain':56,'Aszad':90,'Nawsheen':33}
+
+print(d.keys())
+
+#now you see its not properly as listed , you can turn the whole thing into list 
+
+print(list(d.keys()))
